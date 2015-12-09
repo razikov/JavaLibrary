2fc5679a66586f841dfbe9018fb0b154f351db5d
razikov <razikov88@yandex.ru>
2015-12-09 21:06:02 +0200

    Подключена и работает Hibernate

---
+++ src/library/Book.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+
+/**
+ *
+ * @author razikov
+ */
+public class Book {
+    private Integer id;
+    private String author;
+    private String title;
+    private String isbn;
+    private String genre;
+    private Integer age;
+    private Integer quantity;
+    
+    Book() {
+    }
+    
+    public Integer getId() {
+        return this.id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getAuthor() {
+        return this.author;
+    }
+    
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public String getTitle() {
+        return this.title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getIsbn() {
+        return this.isbn;
+    }
+    
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    
+    public String getGenre() {
+        return this.genre;
+    }
+    
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    public Integer getAge() {
+        return this.age;
+    }
+    
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+    
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
